0eed94f5a610ef2cc622847a2e2265a9889e6c67
Nolan Minar <nolanminar@Nolans-MacBook-Air.local>
2019-08-30 20:34:42 -0500

    final changes,toggle red

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-//
-//
-//
+//this script takes in input from the DOM
+//it then creates an object and uses that information to display the information on the DOM as well as monthly cose
+//if you hit the delete button, the monthly cost will be changed and the employee will be deleted
 $(document).ready(readyNow);
-let totalCost = 0
+let totalCost = 0;
+let employees = [];
 function readyNow(){
     $('#submitBtn').on('click',addEmployee)
 }
@@ -22,18 +23,16 @@ function addEmployee(){
     }
     let $tr = `<tr id="${newObject.empSalary}" ><td>${newObject.fName}</td><td>${newObject.lName}</td><td>${newObject.idNum}</td><td>${newObject.empTitle}</td><td>${newObject.empSalary}</td><td><button id="deleteBtn" onclick="removeEmp(this)">Delete</button></tr>`
     $('#tableBody').append($tr);
+    employees.push(newObject);
     totalCost += (parseInt(newObject.empSalary)) / 12;
-    // let month = totalCost.reduce((a,b) => a+b,0) / 12;
     let $mCost = `<h3>Monthly Cost: ${totalCost}</h3>`
     let dataId = '#'+newObject.empSalary
     $(dataId).data('empId',newObject.empSalary);
-    console.log($(dataId).data('empId'));
     $('#monthCost').empty();
     $('#monthCost').append($mCost);
-    if(totalCost>=20000){
+    if(totalCost>20000){
         $('#monthCost').toggleClass('red');
     }
-    console.log(newObject);
     $('#empFirst').val('');
     $('#empLast').val('');
     $('#empNum').val('');
@@ -42,11 +41,12 @@ function addEmployee(){
 }
 function removeEmp(who){
     let thatOne = $(who).parent().parent().data("empId");
-    console.log(thatOne);
     totalCost -= thatOne/12;
-    // let month = totalCost.reduce((a,b) => a+b,0) / 12;
     let $mCost = `<h3>Monthly Cost: ${totalCost}</h3>`
     $('#monthCost').empty();
     $('#monthCost').append($mCost);
     $(who).parent().parent().remove();
+    if(totalCost<=20000){
+        $('.red').toggleClass('red');
+    }
 }
